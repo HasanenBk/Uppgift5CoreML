af2f34940502446eeccb1db00207098c3b04b0e8
Hasanen <hasanen.bak@gmail.com>
2023-11-26 23:08:05 +0100

    Initial Commit

---
+++ Uppgift5CoreML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uppgift5CoreML
+//
+//  Created by Hasanen B on 2023-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uppgift5CoreML/Uppgift5CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift5CoreMLApp.swift
+//  Uppgift5CoreML
+//
+//  Created by Hasanen B on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift5CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
