d6867b79d05d51d56e946a390b951e9247076ac0
Hasanen <hasanen.bak@gmail.com>
2023-11-27 01:20:21 +0100

    Uppgift5

--- Uppgift5CoreML/ContentView.swift
+++ Uppgift5CoreML/ContentView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let model = Model()
+    let cat = "cat"
+    let elephant = "elephant"
+    @State var result = ""
+     @State var theImage = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack{
+                Button(action: {
+                    result = model.getImage(theImage: cat) ?? cat
+                    theImage = cat
+                }, label: {
+                    Text(cat)
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.646, saturation: 0.728, brightness: 0.969)/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(10)
+                })
+                
+                Button(action: {
+                    result = model.getImage(theImage: elephant) ?? elephant
+                    theImage = elephant
+                }, label: {
+                    Text(elephant)
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.646, saturation: 0.728, brightness: 0.969)/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(10)
+                })
+            }
+            
+            Spacer()
+            Image(theImage)
+            Spacer()
+            
+            Text(result)
+                .font(.title2)
+                .padding()
+           
         }
         .padding()
     }
